45e98bda1adc8316fd1aafe7d008c91beb679771
goodvibesliza <lizapoodle@gmail.com>
2025-06-22 09:38:23 -0400

    Update RetailerProfile.jsx
    added import storage from firebase at line 6

--- src/components/RetailerProfile.jsx
+++ src/components/RetailerProfile.jsx
@@ -3,8 +3,10 @@ import { useNavigate } from 'react-router-dom'
 import { auth, db, storage } from '../lib/firebase'
 import { doc, getDoc, setDoc, updateDoc, collection, addDoc, getDocs, query, where } from 'firebase/firestore'
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { storage } from '../lib/firebase'
 import { signOut } from 'firebase/auth'
 import { moderateHealthContent, filterPostContent } from '../ContentModeration'
+
 
 // FIXED font styles - Headlines thicker, subheadings Inter
 const fontStyles = {
